a73db76cda129444413980c18522347666aa09b0
mahesh lamichhane <softwarengineer007@gmail.com>
2023-06-04 01:04:26 +0545

    beer microserves designed

---
+++ beer-microservice/src/main/java/com/lamichhane/beer/service/web/model/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.lamichhane.beer.service.web.model;
+
+public enum BeerStyleEnum {
+
+    ARNA,TUBORG,NEPALICE,GORKHA,STOUT,GOSE,LAGER,PILSNER,PORTER,IPA
+}
---
+++ beer-microservice/src/test/java/com/lamichhane/beer/service/BeerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamichhane.beer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
